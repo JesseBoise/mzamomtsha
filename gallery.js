e77df35da4add987806074b7e52bd26fcb159a54
Jesse <jesseb727@gmail.com>
2020-03-19 15:31:42 +0200

    Header & Gallery

---
+++ gallery.js
@@ -0,0 +1,61 @@
+var imageNames = [];
+
+var indicators = document.querySelectorAll(".gallery-wrapper .gallery-indicators img");
+for (var i = 0; i < indicators.length; i++) {
+    imageNames.push(indicators[i].src);
+}
+
+currentSlide = 0;
+maxSlides = 5;
+
+window.onload = function () {
+    setupSlider();
+};
+
+function setupSlider() {
+    var slide = document.getElementById('gallery-slide');
+    var controlLeft = document.getElementsByClassName('gallery-control-prev')[0];
+    var controlRight = document.getElementsByClassName('gallery-control-next')[0];
+
+    setSlide(0);
+
+    var slideTimer = setInterval(function() {
+        changeSlide(1);
+    }, 10000);
+
+    controlLeft.addEventListener('click', function () {
+        changeSlide(-1);
+        clearInterval(slideTimer);
+        slideTimer = setInterval(function() {
+            changeSlide(1);
+        }, 1000);
+    });
+    controlRight.addEventListener('click', function() {
+        changeSlide(1);
+        clearInterval(slideTimer);
+        slideTimer = setInterval(function() {
+            changeSlide(1);
+        }, 1000);
+    });
+
+    function changeSlide(byAmount) {
+        currentSlide += byAmount;
+
+        if (currentSlide < 0 || currentSlide >= maxSlides) {
+            currentSlide = Math.abs(currentSlide) - (Math.floor(Math.abs(currentSlide) / maxSlides) * maxSlides);
+        }
+
+        slide.src = imageNames[currentSlide];
+    }
+    function setSlide(byAmount) {
+        currentSlide = byAmount;
+
+        if (currentSlide < 0) {
+            currentSlide = 0;
+        } else if (currentSlide >= maxSlides) {
+            currentSlide = maxSlides;
+        }
+
+        slide.src = imageNames[currentSlide];
+    }
+}
