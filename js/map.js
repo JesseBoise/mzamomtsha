85963e580c8403c1a295f0eed47cecd8f2eb4533
Jesse <jesseb727@gmail.com>
2020-03-20 13:59:13 +0200

    Add GMaps api code and comment out

---
+++ js/map.js
@@ -0,0 +1,14 @@
+// TODO: Fix API errors, so that it can be implemented
+
+// function initMap() {
+//     var coordinates = { lat: -34.003386, lng: 18.651124 };
+//     var map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 12,
+//         center: coordinates
+//     });
+
+//     var marker = new google.maps.Marker({
+//         position: coordinates,
+//         map: map
+//     });
+// }
